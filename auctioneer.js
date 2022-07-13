c3495bfc2f4de48e46af1830f3adc90073e31cc3
Maverick9081 <faliyaabhishek@gmail.com>
2022-07-13 19:02:14 +0530

    Full auctioneer

--- auctioneer.js
+++ auctioneer.js
@@ -16,12 +16,12 @@ import pkgg,{ Metadata }from "@metaplex-foundation/mpl-token-metadata"
 
 async function by(){
   
-const key = [201,127,6,149,29,164,83,181,23,2,115,92,244,206,178,80,20,119,95,3,204,246,101,39,167,170,218,241,99,212,157,182,143,90,61,168,156,64,146,244,233,199,175,141,157,82,105,239,106,66,192,46,179,146,240,230,249,137,89,255,64,139,2,139]
-  // const key = [51,2,34,195,173,249,234,30,34,12,67,162,12,127,33,117,228,99,104,60,105,105,181,163,158,216,91,223,183,97,176,20,49,116,67,172,8,62,193,104,116,116,93,44,37,69,192,52,244,218,171,128,127,107,188,46,106,189,22,24,50,46,218,166]
+// const key = [201,127,6,149,29,164,83,181,23,2,115,92,244,206,178,80,20,119,95,3,204,246,101,39,167,170,218,241,99,212,157,182,143,90,61,168,156,64,146,244,233,199,175,141,157,82,105,239,106,66,192,46,179,146,240,230,249,137,89,255,64,139,2,139]
+  const key = [51,2,34,195,173,249,234,30,34,12,67,162,12,127,33,117,228,99,104,60,105,105,181,163,158,216,91,223,183,97,176,20,49,116,67,172,8,62,193,104,116,116,93,44,37,69,192,52,244,218,171,128,127,107,188,46,106,189,22,24,50,46,218,166]
     const connection = new Connection(clusterApiUrl("devnet"));
     const wallet =  Keypair.fromSecretKey(await Uint8Array.from(key));
-    // const mint = new PublicKey("3ay5NzcEVuVCfPzfjcnRiaYv8KbYNFEPuBGBJ3Mp4Y5u")
-    const mint = new PublicKey("5pYtBVsRJrfztmA5FqNw5AL1o2RH48Bhfc7qKMEmcifP")
+    const mint = new PublicKey("HFZMyVbrVp9sw4aBJ9BP6RvSnqXmiSfR4LJfvTShUBa3")
+    // const mint = new PublicKey("5pYtBVsRJrfztmA5FqNw5AL1o2RH48Bhfc7qKMEmcifP")
     const aH = new PublicKey("BnHNmwRwMHpjq9LBkvQYTkMGRAY4yuWcT5nnGhVq4SBr")
 
     const TOKEN_METADATA_PROGRAM_ID = new PublicKey(
@@ -39,7 +39,7 @@ const key = [201,127,6,149,29,164,83,181,23,2,115,92,244,206,178,80,20,119,95,3,
       const publicKey = wallet.publicKey
 
       
-      const AUCTIONEER = new PublicKey("neer8g6yJq2mQM6KbnViEDAD4gr3gRZyMMf4F2p3MEh")
+      const AUCTIONEER = new PublicKey("86pG2PQ9xdyFkjXthi29MdXEx8a3kifBfzT4YeJ6ytce")
       const auctioneerAuthority =  await PublicKey.findProgramAddress([Buffer.from('auctioneer'),
                                                             aH.toBuffer()]
                                                             ,AUCTIONEER);
@@ -54,24 +54,27 @@ const key = [201,127,6,149,29,164,83,181,23,2,115,92,244,206,178,80,20,119,95,3,
 
                                               
 // Authorize auctioneer
-      //  const authorizeKeys = {
-      //   wallet: publicKey,
-      //   auctionHouse: aH,
-      //   auctioneerAuthority: auctioneerAuthority[0],
-      // }
+ const authorizeKeys = {
+        wallet: publicKey,
+        auctionHouse: aH,
+        auctioneerAuthority: auctioneerAuthority[0],
+      }
 
-      // const authorizeIns = await auctioneer.createAuthorizeInstruction(authorizeKeys);
+      const authorizeIns = await auctioneer.createAuthorizeInstruction(authorizeKeys);
 
-      // let ix = new Transaction();
-      // ix.add(authorizeIns);
-      // ix.recentBlockhash = (await connection.getLatestBlockhash()).blockhash
-      // ix.feePayer = publicKey
-      // await ix.sign(wallet);
+      let ix = new Transaction();
+      ix.add(authorizeIns);
+      ix.recentBlockhash = (await connection.getLatestBlockhash()).blockhash
+      ix.feePayer = publicKey
+      ix.instructions[0].keys[3]
+      ix.instructions[0].programId = AUCTIONEER
+      console.log(ix.instructions[0].keys[3].pubkey.toBase58());
+      await ix.sign(wallet);
 
-      // const sign = await connection.sendRawTransaction(ix.serialize());
+      const sign = await connection.sendRawTransaction(ix.serialize());
 
-      // const tra = await connection.confirmTransaction(sign, 'confirmed');
-      // console.log(tra);
+      const tra = await connection.confirmTransaction(sign, 'confirmed');
+      console.log(tra);
 
       let delegateIns = {
           auctionHouse: aH,
@@ -250,32 +253,82 @@ const key = [201,127,6,149,29,164,83,181,23,2,115,92,244,206,178,80,20,119,95,3,
       programAsSignerBump: signerBump,
       auctioneerAuthorityBump: 254,
       tokenSize: new BN(Math.ceil(1 * 1)),
-      startTime: 1657691565,
-      endTime: 1677534999,
+      startTime: 1657714393,
+      endTime: 1657714599,
       reservePrice: 1,
       minBidIncrement: 1,
-      timeExtPeriod: 10000,
-      timeExtDelta: 1000,
+      timeExtPeriod: 1,
+      timeExtDelta: 1,
       allowHighBidCancel: true
     }
     // console.log(args)
     const sellInstruction  =  await createSellInstruction(accounts,args)
 
-   let tx = new Transaction();
-      tx.add(sellInstruction);
-      tx.recentBlockhash = (await connection.getLatestBlockhash()).blockhash
+  //  let tx = new Transaction();
+  //     tx.add(sellInstruction);
+  //     tx.recentBlockhash = (await connection.getLatestBlockhash()).blockhash
      
-       tx.feePayer = publicKey
+  //      tx.feePayer = publicKey
        
-       tx.sign(wallet);
+  //      tx.sign(wallet);
       
-      const signature = await connection.sendRawTransaction(tx.serialize());
+  //     const signature = await connection.sendRawTransaction(tx.serialize());
 
-      const Transact = await connection.confirmTransaction(signature, 'confirmed');
+  //     const Transact = await connection.confirmTransaction(signature, 'confirmed');
 
-      console.log(Transact);
+  //     console.log(Transact);
 
-//Bid
+
+// Deposit 
+
+const [escrowPaymentAccount, escrowPaymentBump] = await PublicKey.findProgramAddress(
+  [
+    Buffer.from('auction_house'),
+    aH.toBuffer(),
+    publicKey.toBuffer(),
+  ],
+  AUCTION_HOUSE_PROGRAM_ID
+)
+
+const depositArgs = {
+  escrowPaymentBump: escrowPaymentBump,
+  auctioneerAuthorityBump: auctioneerAuthority[1],
+  amount: 1000000
+}
+
+const depositAccounts = {
+  auctionHouseProgram: AUCTION_HOUSE_PROGRAM_ID,
+  wallet: publicKey,
+  paymentAccount: publicKey,
+  transferAuthority: publicKey,
+  escrowPaymentAccount: escrowPaymentAccount,
+  treasuryMint: WRAPPED_SOL_MINT,
+  authority: new PublicKey("4L3oWp4ANModX1TspSSetKsB8HUu2TiBpuqj5FGJonAh"),
+  auctionHouse: aH,
+  auctionHouseFeeAccount: feePayer[0],
+  auctioneerAuthority: auctioneerAuthority[0],
+  ahAuctioneerPda: pda[0]
+}
+
+const deposit = await auctioneer.createDepositInstruction(depositAccounts,depositArgs)
+//  let tx = new Transaction();
+//       tx.add(deposit);
+//       tx.recentBlockhash = (await connection.getLatestBlockhash()).blockhash
+     
+//        tx.feePayer = publicKey
+       
+//        tx.sign(wallet);
+      
+//       const signature = await connection.sendRawTransaction(tx.serialize());
+
+//       const Transact = await connection.confirmTransaction(signature, 'confirmed');
+
+//       console.log(Transact);
+
+//execute sale
+
+
+
 
 
 
---
+++ constants.js
@@ -0,0 +1,10 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const TOKEN_METADATA_PROGRAM_ID = new PublicKey('metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s');
+
+export const WRAPPED_SOL_MINT = new PublicKey('So11111111111111111111111111111111111111112');
+ 
+export const AUCTION_HOUSE_PROGRAM_ID = new PublicKey('hausS13jsjafwWwGqZTUQRmWyvyxn9EQpqMwV1PBBmk');
+
+export const AUCTIONEER = new PublicKey("neer8g6yJq2mQM6KbnViEDAD4gr3gRZyMMf4F2p3MEh")
+
